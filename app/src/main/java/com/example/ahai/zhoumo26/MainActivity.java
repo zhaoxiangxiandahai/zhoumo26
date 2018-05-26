56f7017752a3a2933dd98919b12e667e5354087c
zhaoxiangxiandahai <38877001+zhaoxiangxiandahai@users.noreply.github.com>
2018-05-26 13:00:32 +0800

    第一次提交5月26日

---
+++ app/src/main/java/com/example/ahai/zhoumo26/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ahai.zhoumo26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < 5; i++) {
+
+        }
+    }
+}
